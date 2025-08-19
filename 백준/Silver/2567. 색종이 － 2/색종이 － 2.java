809704c0252a904a0893271e7a3d0ffcc38df9c2
Hyeonsu KIM <wshyhs@gmail.com>
2025-08-19 17:51:55 +0900

    [Silver IV] Title: 색종이 - 2, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -11,27 +11,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        ArrayList <Integer> r_list =new ArrayList<>();
-        ArrayList <Integer> c_list = new ArrayList<>();
+        // ArrayList <Integer> r_list =new ArrayList<>();
+        // ArrayList <Integer> c_list = new ArrayList<>();
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
-            r_list.add(r);
+            // r_list.add(r);
             int c = Integer.parseInt(st.nextToken());
-            c_list.add(c);
+            // c_list.add(c);
             for(int j=r; j<r+10; j++){
                 for(int k =c; k<c+10; k++){
                     board[j][k]=1;
                 }
             }
         }
-        Collections.sort(r_list);
-        Collections.sort(c_list);
+        // Collections.sort(r_list);
+        // Collections.sort(c_list);
         int [] dx ={-1,1,0,0};
         int [] dy ={0,0,1,-1};
         int cnt=0;
-        for(int i=r_list.get(0); i<r_list.get(N-1)+10; i++){
-            for(int j=c_list.get(0); j<c_list.get(N-1)+10; j++){
+        for(int i=0; i<100; i++){
+            for(int j=0; j<100; j++){
                 if(board[i][j]==1){
                     for(int k=0; k<4; k++){
                         int nx = i + dx[k];
