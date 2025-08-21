52eee28f436fb101d6a792c104d12496064fc154
Hyeonsu KIM <wshyhs@gmail.com>
2025-08-21 15:54:05 +0900

    [Gold IV] Title: 알파벳, Time: 784 ms, Memory: 12028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
     static int R,C;
     static char[][] board;
-    static boolean[] visited;
+    // static boolean[] visited;
     static int max =0;
 
     public static void main(String[] args)throws IOException {
@@ -21,27 +21,31 @@ public class Main {
                 board[i][j] = line.charAt(j);
             }
         }
-        visited = new boolean[26];
-        visited[board[0][0]-'A']=true;
-        dfs(0, 0,1);
+        // visited = new boolean[26];
+        // visited[board[0][0]-'A']=true;
+        dfs(0, 0,1, 1 << board[0][0]-'A');
         System.out.println(max);
 
 
     }
     static int[] dx = {0,0,1,-1};
     static int[] dy = {1,-1,0,0};
-    static void dfs(int r, int c, int cnt){
-        max = Math.max(cnt, max);
-        for(int i=0; i<4; i++){
+    static void dfs(int r, int c, int cnt, int bitmask) {
+        max = Math.max(max, cnt);
+
+        for (int i = 0; i < 4; i++) {
             int nx = r + dx[i];
             int ny = c + dy[i];
-            if(nx<0 || ny<0 || nx>=R || ny>=C || visited[board[nx][ny]-'A']) continue;
-            visited[board[nx][ny]-'A']=true;
-            dfs(nx, ny,cnt+1);
-            visited[board[nx][ny]-'A']=false;        
+
+            if(nx<0 || ny<0 || nx>=R || ny>=C) continue;
+            int nextChar = board[nx][ny] - 'A';
+
+            if((bitmask & (1 << nextChar)) == 0) {
+                dfs(nx, ny, cnt + 1, bitmask | (1 << nextChar));
+            }
         }
-        
     }
+
 
 
 }
