d8b6baf407cf9d2eb8f5c8868183fa73e688cb04
Hyeonsu KIM <wshyhs@gmail.com>
2025-08-25 12:24:01 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 105 ms, Memory: 27,648 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    static int N,M;
+    static int[] parents;
+    static int[] rank;
+
+    static void make(){
+        for(int i=1; i<=N; i++){
+            parents[i] = i;
+        }
+    }
+
+    static int find(int a){
+        if(parents[a]==a) return a;
+        return parents[a]=find(parents[a]);
+    }
+
+    static boolean union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if(rootA == rootB) return false;
+        else{
+            if(rank[rootA] < rank[rootB]){
+                parents[rootA] = rootB;
+            }
+            else if(rank[rootA] > rank[rootB]){
+                parents[rootB] = rootA;
+            }
+            else{
+                parents[rootA] = rootB;
+                rank[rootB]++;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        for(int tc=1 ; tc<=T; tc++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            parents = new int[N+1];
+            rank = new int[N+1];
+            make();
+
+            for(int i=0; i<M; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                union(a, b);
+            }
+            Set<Integer> group = new HashSet<>();
+            for(int i=1; i<=N; i++){
+                group.add(find(i));
+            }
+            System.out.printf("#%d %d\n",tc,group.size());
+        }
+    }
+
+}
