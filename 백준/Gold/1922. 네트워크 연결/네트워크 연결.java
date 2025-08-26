b1c3f1c30da567d4f61226ecf28b437408e33edd
Hyeonsu KIM <wshyhs@gmail.com>
2025-08-26 12:28:21 +0900

    [Gold IV] Title: 네트워크 연결, Time: 428 ms, Memory: 46036 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Edge implements Comparable<Edge>{
+        int from,to,weight;
+
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+        
+        @Override
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
+        }
+    }
+
+    static int V,E;
+    static int[] parents;
+    static Edge[] edgeList;
+    static void make(){
+        for(int i=1; i<=V; i++){
+            parents[i]= i;
+        }
+    }
+
+    static int find(int a){
+        if(a == parents[a]) return a;
+        return parents[a] = find(parents[a]);
+    }
+
+    static boolean union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+
+        if(aRoot == bRoot) return false;
+        parents[bRoot] = aRoot;
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        E = Integer.parseInt(st.nextToken());
+        parents = new int[V+1];
+        edgeList = new Edge[E];
+        make();
+        for(int i=0; i<E; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            if(from==to){
+                edgeList[i] = new Edge(from, to, 0);
+            }
+            else{
+                edgeList[i] = new Edge(from, to, weight);
+            }
+            
+        }
+        Arrays.sort(edgeList);
+
+        int cnt =0;
+        long total_weight=0;
+        for(Edge e : edgeList){
+            if(union(e.from, e.to)){
+                total_weight += e.weight;
+                if(++cnt == V-1) break;
+            }
+
+        }
+        System.out.println(total_weight);
+    }
+}
