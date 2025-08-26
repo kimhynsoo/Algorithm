303228cfc72e1e9bf8aa4a173ae1d157a3ded96e
Hyeonsu KIM <wshyhs@gmail.com>
2025-08-26 14:42:21 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 212 ms, Memory: 78,144 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,3 +1,4 @@
+//swea 1251. 하나로
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,16 +8,16 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static class island{
-        double x,y;
+    static class island{ //섬의 위치 정보를 저장
+        double x,y; //섬의 좌표 저장
         island(){};
 
-        double length(island o){
+        double distnace(island o){ //유클리드 공식을 이용한 두 점 사이의 거리를 구하는 메서드
             return Math.sqrt(Math.pow(this.x-o.x, 2)+Math.pow(this.y-o.y, 2));
         }
     }
 
-    static class Edge implements Comparable<Edge> {
+    static class Edge implements Comparable<Edge> { //간선의 정보를 저장
         int from,to;
         double weight;
 
@@ -27,7 +28,7 @@ public class Solution {
         }
 
         @Override
-        public int compareTo(Edge o) {
+        public int compareTo(Edge o) { //가중치를 이용하여 오름차 순 정렬
             // TODO Auto-generated method stub
             return Double.compare(this.weight, o.weight);
         }
@@ -36,63 +37,67 @@ public class Solution {
         
     }
 
-    static int V;
-    static double E;
-    static int[] parents;
-    static island[] islandList;
-    // static ArrayList<Edge> edgeList;
-    static PriorityQueue<Edge> edgeList;
-    static int[] visited = new int[2];
+    static int V; //섬의 개수(정점의 개수)
+    static double E; //환경 부담 세율
+    static int[] parents; //각 정점의 부모 저장.
+    static island[] islandList; //섬의 좌표 저장 배열
+    // static ArrayList<Edge> edgeList; 
+    static PriorityQueue<Edge> edgeList; //간선의 정보를 저장하는 큐
+    static int[] visited = new int[2]; //조합을 위한 visited배열
 
-    static void make(){
+    static void make(){ //부모 배열 초기화
         for(int i=0; i<V; i++){
             parents[i]=i;
         }
     }
 
-    static int find(int a){
+    static int find(int a){ //정점 a의 최상위 부모(루트)를 찾음
         if(a == parents[a]) return a;
         return parents[a] = find(parents[a]);
     }
 
-    static boolean union(int a, int b){
+    static boolean union(int a, int b){ //두 정점 a와 b를 하나의 집합으로 합침.
         int aRoot = find(a);
         int bRoot = find(b);
-        if(aRoot == bRoot) return false;
-        parents[bRoot] = aRoot;
+        if(aRoot == bRoot) return false; //이미 같은 집합이면 사이클 발생으로 false
+        parents[bRoot] = aRoot; //다른 집합이라면 하나로 합치고 true
         return true;
     }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
+        
+        int T = Integer.parseInt(br.readLine());
 
         for(int tc = 1 ; tc<=T; tc++){
-            st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
+            
+            V = Integer.parseInt(br.readLine());
             parents = new int[V];
-            make();
+            make(); //부모 배열 초기화
             islandList = new island[V];
-            for(int i=0; i<V; i++){
+            for(int i=0; i<V; i++){ //섬 좌표 배열 초기화.
                 islandList[i] = new island();
             }
-            st = new StringTokenizer(br.readLine());
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+            // 섬들의 x좌표와 y좌표를 각각 입력받음
+            StringTokenizer stX = new StringTokenizer(br.readLine());
+            StringTokenizer stY = new StringTokenizer(br.readLine());
             for(int i=0; i<V; i++){
-                islandList[i].x = Integer.parseInt(st.nextToken());
-                islandList[i].y = Integer.parseInt(st2.nextToken());
+                islandList[i].x = Integer.parseInt(stX.nextToken());
+                islandList[i].y = Integer.parseInt(stY.nextToken());
             }
-            st = new StringTokenizer(br.readLine());
-            E = Double.parseDouble(st.nextToken());
             
+            E = Double.parseDouble(br.readLine());
+            
+
             // edgeList = new ArrayList<>();
-            edgeList = new PriorityQueue<>();
-            comb(0, 0);
+            edgeList = new PriorityQueue<>(); //모든 간선을 저장할 우선 순위 큐 생성
+
+            comb(0, 0); //조합을 이용해 모든 섬들을 쌍으로 묶어 간선을 생성하고 큐에 추가.
             // Collections.sort(edgeList);
-            int cnt=0;
-            double total_weight=0;
-            int n = V-1;
+            int cnt=0; //선택 된 간선 수
+            double total_weight=0; // 총 환경 부담금(MST의 총 가중치)
+            int n = V-1; //MST 는 V-1개의 간선으로 구성.
             // for(Edge e : edgeList){
             //     if(union(e.from, e.to)){
             //         total_weight += e.weight;
@@ -101,10 +106,10 @@ public class Solution {
             // }
             while (!edgeList.isEmpty()) {
                 
-                Edge now =edgeList.poll();
-                if(union(now.from,now.to)) {
-                    total_weight += now.weight;
-                    if(++cnt==n) break;
+                Edge now =edgeList.poll(); //가장 가중치가 낮은 간선을 꺼냄
+                if(union(now.from,now.to)) { //두섬이 아직 연결되어 있지 않다면
+                    total_weight += now.weight; //해당 간선을 선택하고 비용 누적
+                    if(++cnt==n) break; //다 찾으면 종료
                 }
                 
             }
@@ -115,15 +120,20 @@ public class Solution {
     }
 
     static void comb(int cnt, int start){
-        if(cnt == 2){
+        if(cnt == 2){ //2개의 섬이 선택 되었을때
             int from = visited[0];
             int to = visited[1];
-            double weight = Math.pow(islandList[from].length(islandList[to]) ,2)* E;
-            edgeList.offer(new Edge(from, to, weight));
+            //두섬 사이의 거리
+            double distance = islandList[from].distnace(islandList[to]);
 
+            //두섬 사이의 비용
+            double weight = Math.pow(distance, 2) *E;
+
+            //간선 정보를 큐에 추가.
+            edgeList.offer(new Edge(from, to, weight));
             return;
         }
-
+        //재귀를 활용한 조합
         for(int i=start; i<V; i++){
             visited[cnt]=i;
             comb(cnt+1, i+1);
