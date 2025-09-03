513001b047bc0fb3e14ca6583a4815fc3f9d8e07
Hyeonsu KIM <wshyhs@gmail.com>
2025-09-03 16:01:44 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 92 ms, Memory: 12868 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    //11726번과 비슷하다.
+    //이번엔 2*2의 직사각형이 추가되었다.
+    //따라서 2*i일 때 가장 오른쪽에 2칸을 차지하는 경우가 하나 추가 된 것이다.
+    //따라서 dp[i] =dp[i-1] + 2*dp[i-2]이다. 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -8,7 +12,7 @@ public class Main {
         dp[1]=1;
         dp[2]=3;
         for(int i=3; i<=N; i++){
-            dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+            dp[i] = (dp[i-1] + 2*dp[i-2] % 10007 )%10007;
         }
 
         System.out.println(dp[N]);
