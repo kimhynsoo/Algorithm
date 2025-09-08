11b4e198197d2f6e7ddc9b573b80fd7b2ec8d0b0
Hyeonsu KIM <wshyhs@gmail.com>
2025-09-08 13:28:09 +0900

    [Gold III] Title: 앱, Time: 96 ms, Memory: 12144 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -8,7 +8,7 @@ public class Main {
     static int N, W;
     static int[] weight, value;
     static int INF = 1_000_000_000;
-    // static int [][] dp;
+
 
 
     public static void main(String[] args) throws IOException {
@@ -17,52 +17,37 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
-        weight= new int[N+1];
         value = new int[N+1];
-        // dp = new int[N+1][W+1];
+        weight= new int[N+1];
 
 
-        st=new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++){
-            weight[i]= Integer.parseInt(st.nextToken());
-        }
 
         st=new StringTokenizer(br.readLine());
         for(int i=1; i<=N; i++){
             value[i]= Integer.parseInt(st.nextToken());
         }
-        // for(int i=0; i<=N; i++){
-        //     Arrays.fill(dp[i], INF);
-        //     dp[i][0]=0;
-        // }
-        // for(int i=1; i<=N; i++){
-        //     for(int w= weight[i]; w<=W; w++){
-        //         dp[i][w] = dp[i-1][w];
-
-        //         if(dp[i-1][w-weight[i]]!=INF){
-        //             dp[i][w] = Math.min(dp[i][w], dp[i-1][w-weight[i]]+value[i]);
-        //         }
-        //     }
-        // }
-        // System.out.println(dp[N][W]);
-        int sumWeight = 0;
-        for(int i=1; i<=N; i++) sumWeight += weight[i];
-
-        int[] dp = new int[sumWeight+1];
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
-
+        int K=0;
+        st=new StringTokenizer(br.readLine());
         for(int i=1; i<=N; i++){
-            for(int w=sumWeight; w>=weight[i]; w--){
-                dp[w] = Math.min(dp[w-weight[i]] + value[i], dp[w]);
+            weight[i]= Integer.parseInt(st.nextToken());
+            K+=weight[i];
+        }
+
+        int[] dp = new int[K+1];
+        
+        for(int i=1; i<=N; i++){
+            for(int w=K; w-weight[i]>=0; w--){
+                dp[w] = Math.max(dp[w-weight[i]] + value[i], dp[w]);
             }
         }
 
-        int ans = INF;
-        for(int w=W; w<=sumWeight; w++){
-            ans = Math.min(ans, dp[w]);
+        for(int i=0; i<=K; i++){
+            if(dp[i]>=W){
+                System.out.println(i);
+                break;
+            }
         }
 
-        System.out.println(ans);
+        
     }
 }
