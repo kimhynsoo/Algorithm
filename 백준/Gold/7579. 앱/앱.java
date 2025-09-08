b2293e0fcdf9c462297e641ea75e0603a2a0e819
Hyeonsu KIM <wshyhs@gmail.com>
2025-09-08 13:35:09 +0900

    [Gold III] Title: 앱, Time: 92 ms, Memory: 12128 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -7,43 +7,42 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, W;
     static int[] weight, value;
-    static int INF = 1_000_000_000;
 
 
 
+    //M 바이트 이상의 메모리를 추가로 확보해야 한다.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
-        value = new int[N+1];
-        weight= new int[N+1];
+        value = new int[N];
+        weight= new int[N];
 
 
 
         st=new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++){
+        for(int i=0; i<N; i++){
             value[i]= Integer.parseInt(st.nextToken());
         }
-        int K=0;
+        int K=0; //만들 수 있는 최대 비용
         st=new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++){
+        for(int i=0; i<N; i++){
             weight[i]= Integer.parseInt(st.nextToken());
-            K+=weight[i];
+            K+=weight[i];   
         }
 
         int[] dp = new int[K+1];
         
-        for(int i=1; i<=N; i++){
-            for(int w=K; w-weight[i]>=0; w--){
-                dp[w] = Math.max(dp[w-weight[i]] + value[i], dp[w]);
+        for(int i=0; i<N; i++){
+            for(int w=K; w-weight[i]>=0; w--){//중복 방지를 위해서 마지막부터 역순으로 탐색한다.
+                dp[w] = Math.max(dp[w-weight[i]] + value[i], dp[w]);    //w비용으로 만들 수 있는 최대 바이트
             }
         }
 
         for(int i=0; i<=K; i++){
-            if(dp[i]>=W){
-                System.out.println(i);
+            if(dp[i]>=W){ //i 비용으로 만들 수 있는 바이트가 W를 넘으면 출력 후 break
+                System.out.println(i); 
                 break;
             }
         }
