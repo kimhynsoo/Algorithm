8a856700ccdbf8fbc5559cabaf57eae477ce543f
Hyeonsu KIM <wshyhs@gmail.com>
2025-09-11 10:24:30 +0900

    Update Z.java

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,60 +1,72 @@
 import java.util.Scanner;
 
-public class Main {
-    static int cnt=0;
-    static int N,R,C;
+public class boj_1074_Z {
+    static int order = 0;   // 방문 순서(카운트)
+    static int N, targetRow, targetCol; 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 입력: 2^N x 2^N 크기 배열, 목표 좌표 (R, C)
         N = sc.nextInt();
-        R = sc.nextInt();
-        C = sc.nextInt();
-        draw(0, 0, (int)Math.pow(2, N));
-        System.out.println(cnt);
-        
+        targetRow = sc.nextInt();
+        targetCol = sc.nextInt();
+
+        // Z-모양 탐색 시작
+        int size = (int) Math.pow(2, N);  
+        search(0, 0, size);
+
+        System.out.println(order);
     }
-    static boolean end_flag;
-    static void draw(int r, int c, int n){
-        if(n==2){
-            int r_n = r+n/2;
-            int c_n = c+n/2;
-            if(R<r_n && C<c_n){ //1사
-                
+
+    /**
+     * Z-모양 순서 탐색 (분할 정복)
+     * @param row 현재 시작 행
+     * @param col 현재 시작 열
+     * @param size 현재 탐색하는 정사각형 크기
+     */
+    static void search(int row, int col, int size) {
+        // base case: 크기가 2x2인 경우
+        if (size == 2) {
+            int midRow = row + size / 2;
+            int midCol = col + size / 2;
+
+            if (targetRow < midRow && targetCol < midCol) { // 1사분면 (좌상단)
+                // 변화 없음
+            } 
+            else if (targetRow < midRow && targetCol >= midCol) { // 2사분면 (우상단)
+                order += 1;
+            } 
+            else if (targetRow >= midRow && targetCol < midCol) { // 3사분면 (좌하단)
+                order += 2;
+            } 
+            else if (targetRow >= midRow && targetCol >= midCol) { // 4사분면 (우하단)
+                order += 3;
             }
-            else if(R<r_n && C>=c_n){ //2사
-                cnt += 1;
-                
-            }
-            else if(R>=r_n && C<c_n){ //3사
-                cnt += 2;
-                
-            }
-            else if(R>=r_n && C>=c_n){ //4사
-                cnt += 3;
-                
-            }
-        }else{
-            int next_n =n/2;
-            int r_n = r+next_n;
-            int c_n = c+next_n;
-            
-            int plus = (n/2) *(n/2);
-            if(R<r_n && C<c_n){ //1사
-                draw(r, c, next_n);
-            }
-            else if(R<r_n && C>=c_n){ //2사
-                cnt +=  plus;
-                draw(r, c+next_n, next_n);
-            }
-            else if(R>=r_n && C<c_n){ //3사
-                cnt += plus*2;
-                draw(r+next_n, c, next_n);
-            }
-            else if(R>=r_n && C>=c_n){ //4사
-                cnt += plus*3;
-                draw(r+next_n, c+next_n, next_n);
-            }
+            return;
         }
 
-    
+        // 현재 영역을 4분할
+        int half = size / 2;
+        int midRow = row + half;
+        int midCol = col + half;
+        int quadrantSize = half * half; // 한 사분면에 속하는 칸 개수
+
+        // 목표 좌표가 어느 사분면에 있는지 판별
+        if (targetRow < midRow && targetCol < midCol) { // 1사분면 (좌상단)
+            search(row, col, half);
+        } 
+        else if (targetRow < midRow && targetCol >= midCol) { // 2사분면 (우상단)
+            order += quadrantSize;
+            search(row, col + half, half);
+        } 
+        else if (targetRow >= midRow && targetCol < midCol) { // 3사분면 (좌하단)
+            order += quadrantSize * 2;
+            search(row + half, col, half);
+        } 
+        else if (targetRow >= midRow && targetCol >= midCol) { // 4사분면 (우하단)
+            order += quadrantSize * 3;
+            search(row + half, col + half, half);
+        }
     }
 }
