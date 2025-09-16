66f863d0c4c742255edac49e19399da4dd68a884
Hyeonsu KIM <wshyhs@gmail.com>
2025-09-16 11:00:00 +0900

    [Gold IV] Title: 호텔, Time: 104 ms, Memory: 12600 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int [] weights = new int[N];
+        int [] values = new int[N];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+        int [] dp = new int[100000+1]; //N원일 때 최대 사람
+        for(int i=0; i<N; i++){
+            for(int w=weights[i]; w<=100000; w++){
+                dp[w]=Math.max(dp[w],dp[w-weights[i]]+values[i] );
+            }
+        }
+
+        for(int i=0; i<=100000; i++){
+            if (dp[i]>=C){
+                System.out.println(i);
+                break;
+            }
+        }
+
+    }
+}
