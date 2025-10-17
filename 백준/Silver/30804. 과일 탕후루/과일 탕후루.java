9676dfe0cc941a9cfc80bc171527608b62cb1c97
Hyeonsu KIM <wshyhs@gmail.com>
2025-10-17 17:35:59 +0900

    [Silver II] Title: 과일 탕후루, Time: 208 ms, Memory: 28064 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,26 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
         int [] nums = new int[N];
+        st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            nums[i] = sc.nextInt();
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        Map<Integer,Integer> map = new HashMap<>();
-
+        int kindCnt=0;
+        int[] fruit = new int[10];
         int left=0;
         int max_len=0;
         for(int right=0; right<N; right++){
-            map.put(nums[right],map.computeIfAbsent(nums[right],k->0)+1);
-            while (map.size()>2) {
-                map.compute(nums[left], (k,v)->v-1);
-                if(map.get(nums[left])==0) map.remove(nums[left]);
+            if(fruit[nums[right]]==0) kindCnt++;
+            fruit[nums[right]]++;
+            while (kindCnt>2) {
+                fruit[nums[left]]--;
+                if(fruit[nums[left]]==0) kindCnt--;
                 left++;
             }
             max_len=Integer.max(max_len, right-left+1);
