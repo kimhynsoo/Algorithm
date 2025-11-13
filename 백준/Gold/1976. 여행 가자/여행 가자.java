4bbabafb8ed6b290b01accd7f0109e38ec46c52e
Hyeonsu KIM <wshyhs@gmail.com>
2025-11-13 21:00:46 +0900

    [Gold IV] Title: 여행 가자, Time: 124 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static int find(int x){
+        if(parent[x]==x)return x;
+        return parent[x] = find(parent[x]);
+    }
+    static void union(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a!=b) parent[b]=a;
+    }
+    static int []parent;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        
+        parent=new int[N+1];
+        for(int i=1; i<=N; i++){
+            parent[i]=i;        }
+        for(int i=1; i<=N; i++){
+            StringTokenizer st =new StringTokenizer(br.readLine());
+            for(int j=1; j<=N; j++){
+                int num = Integer.parseInt(st.nextToken());
+                if(num==1){
+                    union(i, j);
+                }
+            }
+        }
+
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        for(int i=1; i<M; i++){
+            int to =Integer.parseInt(st.nextToken());
+            if(find(start)!=find(to)){
+                System.out.println("NO");
+                return;
+            }
+            start=to;
+        }
+        System.out.println("YES");
+    }
+    
+}
